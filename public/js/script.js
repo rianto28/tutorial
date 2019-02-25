5d4ccf48ab8e5fe09b824337cb9586401db612ae
rianto28 <rianto10@gmail.com>
2019-02-25 16:55:09 +0700

    tambahan folder public

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+﻿function myFunction() {
+    var x = document.getElementById("fname");
+     x.value = x.value.toLowerCase().replace(/b[a-z]/g, function(letter) {
+    return letter.toUpperCase();
+  });
+}
+
+
+ 
